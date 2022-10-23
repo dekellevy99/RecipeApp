29431671ed32bbd160525bb54ecc30b46aeffa2d
dekellevy99 <dekelllevy@gmail.com>
2022-10-23 12:29:40 +0300

    fix a little bug

--- client/src/Render.ts
+++ client/src/Render.ts
@@ -1,7 +1,7 @@
 const RenderModule = function(){
 
     const renderRecipes = function(data: Data): void{
-        $("#recipes-container").remove()
+        $("#recipes-container").empty()
         let source = $("#recipe-card-template").html()
         let template = Handlebars.compile(source)
         $("#recipes-container").append(template({recipes: data.recipes}))
